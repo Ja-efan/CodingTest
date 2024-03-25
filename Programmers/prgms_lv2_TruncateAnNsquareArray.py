5f6b6e2c5e24fe594dd2bdb4230e6a7e591df6c4
jaefan <woghks1213@gmail.com>
2024-03-25 23:35:02 +0900

    20240325 programmers #87390

---
+++ Programmers/prgms_lv2_TruncateAnNsquareArray.py
@@ -0,0 +1,50 @@
+# 87390
+
+def solution(n, left, right):
+    
+    """
+    n^2 배열 자르기 :
+    정수 n, left, right가 주어집니다. 다음 과정을 거쳐서 1차원 배열을 만들고자 합니다.
+
+    n행 n열 크기의 비어있는 2차원 배열을 만듭니다.
+    i = 1, 2, 3, ..., n에 대해서, 다음 과정을 반복합니다.
+    1행 1열부터 i행 i열까지의 영역 내의 모든 빈 칸을 숫자 i로 채웁니다.
+    1행, 2행, ..., n행을 잘라내어 모두 이어붙인 새로운 1차원 배열을 만듭니다.
+    새로운 1차원 배열을 arr이라 할 때, arr[left], arr[left+1], ..., arr[right]만 남기고 나머지는 지웁니다.
+
+    Args :
+    - n : 1 이상 10^7 이하 
+    - left, right : 0 <= left <= right < n^2, right - left < 10^5
+
+    Returns : 만들어진 1차원 배열 
+
+    """
+    """
+    # soluition 1
+    # 2차원 배열 생성
+    matrix = []
+    for i in range(n):
+        maxx = i+1
+        inner_matrix = []
+        for j in range(n):
+            if j+1 > maxx : maxx= j+1
+            # inner_matrix.append(maxx)
+            matrix.append(maxx)
+        # matrix.append(inner_matrix)
+
+    
+    # print(matrix)
+
+    return matrix[left:right+1]
+    """
+
+    # solution 2
+    low_left, col_left = divmod(left,n)
+    low_right, col_right = divmod(right,n)
+
+    print(low_left, col_left)
+    print(low_right, col_right)
+
+            
+print(solution(3,2,5))
+print(solution(4,7,14))
