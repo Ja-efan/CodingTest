8f583ded38358903ad4555a98f0b08d6a4a5678a
jaefan <woghks1213@gmail.com>
2024-03-29 15:53:20 +0900

    20240329 prgms #42567 add  solution3

--- Programmers/prgms_lv1_AthletesWhoDidNotFinish.py
+++ Programmers/prgms_lv1_AthletesWhoDidNotFinish.py
@@ -49,6 +49,18 @@ def solution(participant:list, completion:list) -> str:
 
     # return participant[0]
 
+    # solution 3 : sorting
+    # idea : participant와 completion을 동일하게 정렬하면 다른 부분이 생김을 이용
+    # completion에 대해서만 1회 순회하므로 O(n)
+        
+    completion = sorted(completion)
+    participant = sorted(participant)
+
+    for i in range(len(completion)):
+        if completion[i] != participant[i]: # 참가자와 완주자의 이름이 같지 않은 경우 해당 참가자는 완주 하지 못한 것이므로 참가자의 이름 반환
+            return participant[i] 
+    
+    return participant[len(participant)-1] # completion의 모든 이름을 순회한 경우 -> 마지막 참가자가 완주하지 못한 선수!!
 
 # test case
 print(solution(["leo", "kiki", "eden", "eden"], ["leo","eden", "kiki"]))
