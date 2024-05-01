8339c83d4b25f0337a28c01c5ef807a32f7ad443
jaefan <woghks1213@gmail.com>
2024-05-01 16:12:33 +0900

    20240501 prgms #43165 타겟넘버

--- Programmers/prgms_lv2_TargetNumber.py
+++ Programmers/prgms_lv2_TargetNumber.py
@@ -7,5 +7,20 @@ def solution(numbers:list, target:int) -> int:
     타겟 넘버 :
 
     """
+    curr = 0
+    idx = 0
 
+    def dfs(nums, target, curr, idx):
+        if idx == len(nums) :
+            if curr == target:
+                return 1
+            else : return 0
+        return dfs(nums, target, curr+nums[idx], idx+1) \
+            + dfs(nums, target, curr-nums[idx], idx+1)
     
+    answer = dfs(nums=numbers, target=target, curr=curr, idx=idx)
+    return answer 
+
+# test case 
+print(solution([1,1,1,1,1] , 3)) # 5
+print(solution([4,1,2,1] , 4)) # 2
