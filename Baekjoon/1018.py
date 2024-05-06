1e8a6b2b731867914ee5e0f02cff2b2a68788de4
jaefan <woghks1213@gmail.com>
2024-05-06 17:20:23 +0900

    20240506 #1018 solved

--- Baekjoon/1018.py
+++ Baekjoon/1018.py
@@ -2,7 +2,7 @@
 # 체스판 다시 칠하기 
 # 1018
 # import numpy as np
-import copy 
+# import copy 
 def solution():
     # 사용자 입력 부분 
     n, m = map(int, input().split(" "))
@@ -16,30 +16,60 @@ def solution():
     counts = []
     for i in range(n-8+1):
         for j in range(m-8+1):
+            # start with 'B'
             new_maps = [[0 for _ in range(8)] for _ in range(8)]
-            # print(f"start index : ({i,j})")
-            count = 0 
-            curr = maps[i][j] 
-            # curr_maps = maps[i:i+8][j:j+8]
-            # print(np.stack(curr_maps, axis=1))
+            count = 0
+            new_maps[0][0] = 'B'
+            curr = new_maps[0][0]
+            if curr != maps[i][j] :
+                count += 1
             for k in range(8):
                 for l in range(8):
                     if k == 0 and l == 0 :
-                        new_maps[i+k][j+l] = curr
                         continue
                     if curr == maps[i+k][j+l]:
-                        count += 1
-                        if maps[i+k][j+l] == "B":
-                            new_maps[i+k][j+l] = "W"
-                        else :
-                            new_maps[i+k][j+l] = "B"
-                    curr = maps[i+k][j+l]
+                            count += 1
+                            if maps[i+k][j+l] == "B":
+                                new_maps[k][l] = "W"
+                            else :
+                                new_maps[k][l] = "B"
+                    else :
+                        new_maps[k][l] = maps[i+k][j+l]
+                    curr = new_maps[k][l]
                     if l == 7:
                         if curr == "W" :
                             curr = 'B'
                         else : 
-                            curr = "W"
-            print(count)
+                            curr = "W"  
+            counts.append(count)
+
+            # start with 'W'
+            new_maps = [[0 for _ in range(8)] for _ in range(8)]
+            count = 0
+            new_maps[0][0] = 'W'
+            curr = new_maps[0][0]
+            if curr != maps[i][j] :
+                count += 1
+            for k in range(8):
+                for l in range(8):
+                    if k == 0 and l == 0 :
+                        continue
+                    if curr == maps[i+k][j+l]:
+                            count += 1
+                            if maps[i+k][j+l] == "B":
+                                new_maps[k][l] = "W"
+                            else :
+                                new_maps[k][l] = "B"
+                    else :
+                        new_maps[k][l] = maps[i+k][j+l]
+                    curr = new_maps[k][l]
+                    if l == 7:
+                        if curr == "W" :
+                            curr = 'B'
+                        else : 
+                            curr = "W"  
+
+    
             counts.append(count)
 
     return min(counts)
