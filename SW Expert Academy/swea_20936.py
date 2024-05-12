e2d3bdbba51ece006283e9a4b8c4f3c6eb3465f2
jaefan <woghks1213@gmail.com>
2024-05-13 00:17:28 +0900

    20240513 #20936 in progress

---
+++ SW Expert Academy/swea_20936.py
@@ -0,0 +1,6 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AY9QUhl6cfQDFAVF&categoryId=AY9QUhl6cfQDFAVF&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=&pageSize=10&pageIndex=1
+# 상자 정렬하기
+# D4
+
+def solution_20936():
+    
