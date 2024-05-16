1683f8a97fcfc021f6b58aaecac6638074932267
jaefan <woghks1213@gmail.com>
2024-05-16 20:25:43 +0900

    20240516 #1206 add solution2 : failed

--- SW Expert Academy/swea_1206.py
+++ SW Expert Academy/swea_1206.py
@@ -1,5 +1,5 @@
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&problemLevel=3&problemLevel=4&contestProbId=AV134DPqAA8CFAYh&categoryId=AV134DPqAA8CFAYh&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=PYTHON&select-1=4&pageSize=10&pageIndex=1
-# [S/W 문제해결 기본] 1읾차 - View
+# [S/W 문제해결 기본] 1일차 - View
 """
 # test case 
 10
@@ -40,13 +40,31 @@ def solution_1206():
             오른쪽(j+1, j+2) 중에 max값
             """
             left = max(tc[j-2], tc[j-1])
-            right= max(tc[j+1], tc[j+2])
+            right = max(tc[j+1], tc[j+2])
             # 왼쪽 혹은 오른쪽 건물의 높이가 같거나 더 높은 경우 
             if left >= height or right >= height:
                 continue
             
             views += height - max(left, right)
         print(f"#{i+1} {views}")
+
+
+def solution_1206_2(): # n번째 테스트 케이스 출력이 n+1번째 입력으로 들어가서 틀린다.
+    # 사용자 입력
+    t = int(input())
+    for tc in range(1, t+1):
+        views = 0
+        buildings = list(map(int, input().split()))
+
+        for i in range(2, len(buildings)-2):
+            height = buildings[i]
+            left = max(buildings[i-2], buildings[i-1])
+            right = max(buildings[i+1], buildings[i+2])
+            if left >= height or right >= height:
+                continue
+            views += height - max(left, right)
+        print(f"#{tc} {views}")
+
 
 solution_1206()
 
