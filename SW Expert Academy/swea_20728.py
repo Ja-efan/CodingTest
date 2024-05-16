b4c19497ef29ee41650dbfaca44123730f2fd6aa
jaefan <woghks1213@gmail.com>
2024-05-16 22:08:04 +0900

    20240516 #20728 add annotation

--- SW Expert Academy/swea_20728.py
+++ SW Expert Academy/swea_20728.py
@@ -3,19 +3,18 @@
 # D3
 
 def solution_20728():
-    t = int(input())
-    for tc in range(1, t+1):
-        n, k = map(int, input().split())
-        candies = list(map(int, input().split()))
-        candies = sorted(candies, reverse=True)
-        diff_list = []
-        for i in range(0,n-k+1):
-            max_candies = candies[i:i+k][0]     
-            min_candies = candies[i:i+k][-1]
-            diff_list.append(max_candies-min_candies)
+    t = int(input())  # 사용자 입력
+    for tc in range(1, t+1):  # 각 테스트 케이스
+        n, k = map(int, input().split())  # 사용자 입력 n:주머니 개수, k:나눠 줄 주머니 개수
+        candies = list(map(int, input().split()))  # 주머니 속 사탕 개수를 나타내는 n개의 정수
+        candies = sorted(candies, reverse=True)  # 내림차순 정렬
+        diff_list = []  # 차이를 담을 리스트
+        for i in range(0,n-k+1):  # 0번 idx부터 n-k번 idx까지 반복
+            max_candies = candies[i:i+k][0]  # [i:i+k] 중 가장 큰 값
+            min_candies = candies[i:i+k][-1]  # [i:i+k] 중 가장 작은 값
+            diff_list.append(max_candies-min_candies)  # 가장 큰 값과 가장 작은 값의 차이
         
-        print(f"#{tc} {min(diff_list)}")
-
+        print(f"#{tc} {min(diff_list)}")  # 차이의 최솟값을 출력
 
 
 solution_20728()
