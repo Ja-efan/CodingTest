369294ef987d1e678584c27a7dac0b0439e64594
jaefan <woghks1213@gmail.com>
2024-05-17 22:05:25 +0900

    20240517 #1244 solving

--- SW Expert Academy/swea_1244.py
+++ SW Expert Academy/swea_1244.py
@@ -1,27 +1,34 @@
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&problemLevel=3&problemLevel=4&contestProbId=AV15Khn6AN0CFAYD&categoryId=AV15Khn6AN0CFAYD&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=PYTHON&select-1=4&pageSize=10&pageIndex=1
-# [S/W 문제해결 응용] 2일차 - 최대 상금
+# 최대 상금
 # D3
 
-def solution_1244():
-    n = int(input()) # a number of test cases
-    test_cases = []
-    
-    for _ in range(n):
-        num, change = map(int, input().split(" "))
-        cards =list(map(int, str(num)))
-        test_cases.append((cards, change))
-    
-    for i, tc in enumerate(test_cases):
-        cards, change = tc 
-        MAX = sorted(cards, reverse=True) # 카드로 구성할 수 있는 최댓값
-        
-        
+def maximize_number(s, k):
+    max_num = [0]
+
+    def dfs(s, k):
+        if k == 0:
+            max_num[0] = max(max_num[0], int("".join(s)))
+            return
+
+        n = len(s)
+        for i in range(n):
+            for j in range(i + 1, n):
+                s[i], s[j] = s[j], s[i]
+                dfs(s, k - 1)
+                s[i], s[j] = s[i], s[j]
+
+    s_list = list(s)
+    dfs(s_list, k)
+    return max_num[0]
 
 
+def solution():
+    T = int(input())
+    for t in range(1, T + 1):
+        s, k = input().split()
+        k = int(k)
+        result = maximize_number(s, k)
+        print(f"#{t} {result}")
 
 
-
-            
-# cards= [1,2,3,4]
-# print(cards.index(max(cards)))
-solution_1244()
+solution()
