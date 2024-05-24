a20e95cda5fbefd9020433a783a4210570b141f0
jaefan <woghks1213@gmail.com>
2024-05-24 16:38:03 +0900

    20240524 #19637 solved

---
+++ Baekjoon/baekjoon_19637.py
@@ -0,0 +1,38 @@
+# https://www.acmicpc.net/problem/19637
+# IF문 좀 대신 써줘
+# Silver III
+
+
+import sys
+
+input = sys.stdin.readline
+output = sys.stdout.write
+
+
+def solution2():
+    N, M = map(int, input().split())
+
+    titles = []
+    for _ in range(N):
+        title, max_ = input().split()
+        titles.append((title, int(max_)))
+
+    powers = []
+    for _ in range(M):
+        powers.append(int(input()))
+
+    for power in powers:
+        left = 0
+        right = N-1
+        while left <= right:
+            mid = (left + right) // 2
+            if power > titles[mid][1]:
+                left = mid + 1
+            else :
+                right = mid -1
+        mid = left
+        output(titles[mid][0]+'\n')
+solution2()
+
+
+    
