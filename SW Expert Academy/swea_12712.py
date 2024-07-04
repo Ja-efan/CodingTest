fc0670ba492dae6c3789da106fb65e813a554899
jaefan <woghks1213@gmail.com>
2024-07-04 21:50:53 +0900

    20240704 #12712 solved

--- SW Expert Academy/swea_12712.py
+++ SW Expert Academy/swea_12712.py
@@ -1,6 +1,7 @@
 # https://swexpertacademy.com/main/talk/solvingClub/problemView.do?solveclubId=AZBIg6i6E1gDFAUu&contestProbId=AXuARWAqDkQDFARa&probBoxId=AZBNDpl6hGQDFAUu&type=USER&problemBoxTitle=%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98+Track+%28%EB%82%9C%EC%9D%B4%EB%8F%84+%EC%A4%91%29&problemBoxCnt=3
 # 파리퇴치
 # D2
+# SSAFY 12기 사전학습 
 
 # import sys 
 
