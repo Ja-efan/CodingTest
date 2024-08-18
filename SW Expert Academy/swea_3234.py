372911a355d1d9d6960effb179eac4daad61120e
jaefan <woghks1213@gmail.com>
2024-08-18 13:43:19 +0900

    fix: 240818 swea_3234 [준환이의 양팔저울] timeout #2 (19/21)

--- SW Expert Academy/swea_3234.py
+++ SW Expert Academy/swea_3234.py
@@ -9,22 +9,22 @@ def recursion(weights, right, left, idx):
         cnt += 1
         return
     # 왼쪽에 추가
-    if not left[idx]:
-        left[idx] = weights[idx]
-        recursion(weights, right, left, idx + 1)
-
-    left[idx] = 0
+    # left[idx] = weights[idx]
+    recursion(weights, right, left+weights[idx], idx + 1)
+    # left[idx] = 0
     # 오른쪽에 추가
-    if sum(right) + weights[idx] <= sum(left):
-        right[idx] = weights[idx]
-        recursion(weights, right, left, idx + 1)
-        right[idx] = 0
+    # if sum(right) + weights[idx] <= sum(left):
+    if right + weights[idx] <= left:
+        # right[idx] = weights[idx]
+        recursion(weights, right+weights[idx], left, idx + 1)
+        # right[idx] = 0
 
 
 def permutation(weights, perm):
     if len(perm) == len(weights):  # 순열이 완성된 시점 -> 해당 순열로 왼쪽 오른쪽 구성해야 함
-        right = [0 for _ in range(len(weights))]
-        left = [0 for _ in range(len(weights))]
+        # right = [0 for _ in range(len(weights))]
+        # left = [0 for _ in range(len(weights))]
+        right, left = 0, 0
         # print(f"perm: {perm}")
         recursion(weights=perm, right=right, left=left, idx=0)
         # print("-" * 100)
