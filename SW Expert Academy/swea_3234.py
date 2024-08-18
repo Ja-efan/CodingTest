ddcaf65a619a655d815d5fc67a360a93e41c2e1b
jaefan <woghks1213@gmail.com>
2024-08-18 13:32:49 +0900

    fix: 240818 swea_3234 [준환이의 양팔저울] timeout

--- SW Expert Academy/swea_3234.py
+++ SW Expert Academy/swea_3234.py
@@ -2,29 +2,36 @@
 # D4
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWAe7XSKfUUDFAUw
 
-
-def recursion(idx, right, left, perm, bp):
+def recursion(weights, right, left, idx):
     global cnt
-    if idx == bp:
+    if idx == len(weights):
+        # print(left, right)
+        cnt += 1
         return
-    for weight in perm:
-        if weight not in right:
-            if sum(right) + weight < sum(left):
-                cnt += 1
-                right.append(weight)
-                new_left = [w for w in perm if w not in right]
-                recursion(idx=idx+1, right=right, left=new_left, perm=perm, bp=bp)
-                right.pop()
+    # 왼쪽에 추가
+    if not left[idx]:
+        left[idx] = weights[idx]
+        recursion(weights, right, left, idx + 1)
+
+    left[idx] = 0
+    # 오른쪽에 추가
+    if sum(right) + weights[idx] <= sum(left):
+        right[idx] = weights[idx]
+        recursion(weights, right, left, idx + 1)
+        right[idx] = 0
+
 
 def permutation(weights, perm):
     if len(perm) == len(weights):  # 순열이 완성된 시점 -> 해당 순열로 왼쪽 오른쪽 구성해야 함
-        right = []
-        left = [w for w in perm]
-        recursion(idx=0, right=right, left=left, perm=perm, bp=len(weights))
+        right = [0 for _ in range(len(weights))]
+        left = [0 for _ in range(len(weights))]
+        # print(f"perm: {perm}")
+        recursion(weights=perm, right=right, left=left, idx=0)
+        # print("-" * 100)
         return
     for i in range(len(weights)):
-        if i not in perm:
-            perm.append(i)
+        if weights[i] not in perm:
+            perm.append(weights[i])
             permutation(weights, perm)
             perm.pop()
     return
@@ -33,7 +40,7 @@ def permutation(weights, perm):
 def main():
     T = int(input())  # test case 개수
     global cnt
-    for tc in range(1, T+1):
+    for tc in range(1, T + 1):
         N = int(input())  # 무게 추 개수
         weights = list(map(int, input().split()))
         perm = []
